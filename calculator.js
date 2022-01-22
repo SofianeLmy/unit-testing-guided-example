94491cf3dcbf15bfcfb4540e1952283e858a746e
Sofiane M <poubellepropre8@gmail.com>
2022-01-22 14:13:07 +0100

    part without bonus

--- calculator.js
+++ calculator.js
@@ -1,14 +1,29 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b=== undefined) {
+    return 0;
+  } else if (b === undefined) {
+    return a;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
-}
+  if (a === undefined && b === undefined) {
+    return 0;
+  } else if (b === undefined) {
+    return a;
+  } else {
+    return a - b;
+  }
+}    
 
 function divide(a, b) {
-  return;
-}
+  if(b === 0){
+    throw new Error('You tried to divide by 0');
+  } else {
+    return a /b ;} 
+  }
 
 function multiply(a, b) {
   return a * b;
